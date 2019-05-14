0d645a02a6ca281f2ce347ea911981c0a5012af2
Eric Johnson <eric.d.johnson@gmail.com>
2019-05-14 09:08:33 -0500

    Refactor to iterate through 24 hour forecast of darksky data

--- src/weather-orbs.js
+++ src/weather-orbs.js
@@ -26,7 +26,7 @@ const settings = {
   attributes: { antialias: true },
   cloudCover: 0,
   precipIntensity: 1.5,
-  noiseMultiplier: 1.0,
+  noiseMultiplier: 1.0
 };
 
 // Your sketch, which simply returns the shader
@@ -143,9 +143,9 @@ const sketch = ({ context }) => {
   scene.add(mesh);
 
   updateWeatherData();
-  setInterval(function() {
-    updateWeatherData();
-  }, 60000);
+  // setInterval(function() {
+  //   updateWeatherData();
+  // }, 60000);
 
   // draw each frame
   return {
@@ -157,7 +157,7 @@ const sketch = ({ context }) => {
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render({ time }) {
+    render({ time, playhead }) {
       mesh.rotation.y = time / 4;
       //mesh.rotation.y = Math.sin(time * (Math.PI / 6));
       mesh.material.uniforms.time.value = time;
@@ -189,8 +189,8 @@ async function getWeatherData(lat, lon) {
 }
 
 function updateWeatherData() {
-  const lat = 41.6734;
-  const lon = -91.75705;
+  const lat = 41.5868;
+  const lon = -93.625;
 
   Promise.all([getWeatherData(lat, lon)]).then(res => {
     //extract data from different APIs
@@ -198,215 +198,217 @@ function updateWeatherData() {
 
     console.log(res[0].data);
 
-    //round to the nearest 10
-    let { temperature } = darksky.currently;
-    let nearestTemps = {
-      floor: Math.floor(temperature / 10) * 10,
-      ceil: Math.ceil(temperature / 10) * 10
-    };
-
-    //our temperature palette has colors every 10 degrees
-    let nearestTempColors = {
-      low: temperaturePalette.filter(item => item.temp == nearestTemps.floor)[0]
-        .color,
-      high: temperaturePalette.filter(item => item.temp == nearestTemps.ceil)[0]
-        .color,
-      gradient: (temperature - nearestTemps.floor) / 10
-    };
-
-    console.log('nearest', nearestTempColors);
-
-    let colorForTemp = colorBetween(
-      nearestTempColors.low,
-      nearestTempColors.high,
-      nearestTempColors.gradient
-    );
-
-    //Hazel color adjustment
-    //colorForTemp = '#a82dff';
-    colorForTemp = color(colorForTemp).hsl();
-    console.log(colorForTemp);
-    settings.weatherColor = colorForTemp.color[0] / 360.0; //convert to radians
-
-    //determine the kind of daylight
-    let suninfo = suncalc.getTimes(new Date(), lat, lon);
-    console.log(suninfo);
-    // pre-dawn
-    if (
-      new Date() >= new Date(suninfo.nightEnd) &&
-      new Date() < new Date(suninfo.dawn)
-    ) {
-      settings.daylightValue = 2.3;
-      settings.saturation = 0.2;
+    for (let i = 0; i < 24; i++) {
+      setTimeout(function timer() {
+        renderTheWeather(darksky.hourly.data[i], lat, lon);
+      }, i * 1000);
     }
-    // dawn
-    else if (
-      new Date() >= new Date(suninfo.dawn) &&
-      new Date() < new Date(suninfo.sunrise)
-    ) {
-
-      const dawnDuration = differenceInSeconds(
-        new Date(suninfo.sunrise),
-        new Date(suninfo.dawn)
-      );
-      const dawnProgress = differenceInSeconds(
-        new Date(),
-        new Date(suninfo.sunrise)
-      );
-
-      const progress = eases.cubicIn(dawnProgress / dawnDuration);
-      settings.daylightValue = lerp(2.3, 1.8, progress);
-      settings.saturation = lerp(0.2, 0.6, progress);
-    }
-    // sunrise
-    else if (
-      new Date() >= new Date(suninfo.sunrise) &&
-      new Date() < new Date(suninfo.sunriseEnd)
-    ) {
-      const sunriseDuration = differenceInSeconds(
-        new Date(suninfo.sunriseEnd),
-        new Date(suninfo.sunrise)
-      );
-      const sunriseProgress = differenceInSeconds(
-        new Date(),
-        new Date(suninfo.sunrise)
-      );
-
-
-      const progress = eases.cubicOut(sunriseProgress / sunriseDuration);
-
-      settings.daylightValue = lerp(1.8, 1.2, progress);
-      settings.saturation = lerp(0.6, 0.7, progress);
-
-    }
-    // golden hour
-    else if (
-      new Date() >= new Date(suninfo.sunriseEnd) &&
-      new Date() < new Date(suninfo.goldenHourEnd)
-    ) {
-      settings.daylightValue = 1.2;
-      settings.saturation = 0.7;
-    }
-    // morning
-    else if (
-      new Date() >= new Date(suninfo.goldenHourEnd) &&
-      new Date() < new Date(suninfo.solarNoon)
-    ) {
-      settings.daylightValue = 1.0;
-      settings.saturation = 1.0;
-    }
-    // afternoon
-    else if (
-      new Date() >= new Date(suninfo.solarNoon) &&
-      new Date() < new Date(suninfo.goldenHour)
-    ) {
-      settings.daylightValue = 1.0;
-      settings.saturation = 1.0;
-    }
-    // golden hour
-    else if (
-      new Date() >= new Date(suninfo.goldenHour) &&
-      new Date() < new Date(suninfo.sunsetStart)
-    ) {
-      settings.daylightValue = 1.0;
-      settings.saturation = 1.0;
-    }
-    // sunset
-    else if (
-      new Date() >= new Date(suninfo.sunsetStart) &&
-      new Date() < new Date(suninfo.sunset)
-    ) {
-      const secondsBetween = differenceInSeconds(
-        new Date(suninfo.sunset),
-        new Date(suninfo.sunsetStart)
-      );
-      const secondsOfProgress = differenceInSeconds(
-        new Date(),
-        new Date(suninfo.sunsetStart)
-      );
-      const progress = secondsOfProgress / secondsBetween;
-
-      settings.daylightValue = lerp(1.0, 1.4, progress);
-      settings.saturation = 1.0;
-    }
-    // twilight
-    else if (
-      new Date() >= new Date(suninfo.sunset) &&
-      new Date() < new Date(suninfo.dusk)
-    ) {
-      settings.daylightValue = 1.4;
-      settings.saturation = 0.3;
-    }
-    // dusk
-    else if (
-      new Date() >= new Date(suninfo.dusk) &&
-      new Date() < new Date(suninfo.night)
-    ) {
-      settings.daylightValue = 2.0;
-      settings.saturation = 0.3;
-    }
-    // night
-    else {
-      settings.daylightValue = 2.5;
-      settings.saturation = 0.2;
-    }
-
-
-
-    // Cloud Clover
-    let { cloudCover } = darksky.currently;
-    let hslLightnessForBg = 95.0;
-    settings.cloudCover = Math.round((1 - cloudCover) * hslLightnessForBg);
-
-    // Precipitation
-    let { precipIntensity } = darksky.currently;
-
-    if (precipIntensity === 0) {
-      settings.precipIntensity = 1.0;
-    } else if (precipIntensity < 0.03) {
-      settings.precipIntensity = lerp(
-        2.8,
-        3.0,
-        calculateProgress(0, 0.03, precipIntensity)
-      );
-    } else if (precipIntensity >= 0.03 && precipIntensity < 0.1) {
-      settings.precipIntensity = lerp(
-        3.0,
-        4.0,
-        calculateProgress(0.03, 0.1, precipIntensity)
-      );
-    } else if (precipIntensity >= 0.1 && precipIntensity < 0.2) {
-      let progress = calculateProgress(0.1, 0.2, precipIntensity);
-      settings.precipIntensity = lerp(
-        3.0,
-        3.5,
-        progress
-      );
-      settings.noiseMultiplier = lerp(1.5, 2.0, progress);
-    } else {
-      let precipMultiplier = (precipIntensity / 0.2)
-      settings.precipIntensity = 3.5 * precipMultiplier;
-      settings.noiseMultiplier = 2.0 * precipMultiplier;
-    }
-
-    // Wind Speed
-    let { windSpeed } = darksky.currently;
-    if (windSpeed < 5) {
-      settings.windSpeed = lerp(0, 0.25, calculateProgress(0, 5, windSpeed));
-    } else if (windSpeed >= 5 && windSpeed < 10) {
-      settings.windSpeed = lerp(0.25, 0.45, calculateProgress(5, 10, windSpeed));
-    } else if (windSpeed >= 10 && windSpeed < 20) {
-      settings.windSpeed = lerp(0.45, 1.0, calculateProgress(10, 20, windSpeed));
-    } else if (windSpeed >= 20 && windSpeed < 30) {
-      settings.windSpeed = lerp(1.0, 2.5, calculateProgress(20, 30, windSpeed));
-    } else {
-      settings.windSpeed = 3.5;
-      let windMultiplier = windSpeed / 30;
-      settings.windSpeed = 3.5 * windMultiplier;
-    }
-
     console.log(settings);
   });
+}
+
+function renderTheWeather(darksky, lat, lon) {
+  //round to the nearest 10
+  let { apparentTemperature : temperature } = darksky;
+  let nearestTemps = {
+    floor: Math.floor(temperature / 10) * 10,
+    ceil: Math.ceil(temperature / 10) * 10
+  };
+
+  //our temperature palette has colors every 10 degrees
+  let nearestTempColors = {
+    low: temperaturePalette.filter(item => item.temp == nearestTemps.floor)[0]
+      .color,
+    high: temperaturePalette.filter(item => item.temp == nearestTemps.ceil)[0]
+      .color,
+    gradient: (temperature - nearestTemps.floor) / 10
+  };
+
+  console.log("nearest", nearestTempColors);
+
+  let colorForTemp = colorBetween(
+    nearestTempColors.low,
+    nearestTempColors.high,
+    nearestTempColors.gradient
+  );
+
+  //Hazel color adjustment
+  //colorForTemp = '#a82dff';
+  colorForTemp = color(colorForTemp).hsl();
+  console.log(colorForTemp);
+  settings.weatherColor = colorForTemp.color[0] / 360.0; //convert to radians
+
+  //determine the kind of daylight
+  let suninfo = suncalc.getTimes(
+    new Date(),
+    lat,
+    lon
+  );
+  console.log(suninfo);
+  // pre-dawn
+  if (
+    new Date() >= new Date(suninfo.nightEnd) &&
+    new Date() < new Date(suninfo.dawn)
+  ) {
+    settings.daylightValue = 2.3;
+    settings.saturation = 0.2;
+  }
+  // dawn
+  else if (
+    new Date() >= new Date(suninfo.dawn) &&
+    new Date() < new Date(suninfo.sunrise)
+  ) {
+    const dawnDuration = differenceInSeconds(
+      new Date(suninfo.sunrise),
+      new Date(suninfo.dawn)
+    );
+    const dawnProgress = differenceInSeconds(
+      new Date(),
+      new Date(suninfo.sunrise)
+    );
+
+    const progress = eases.cubicIn(dawnProgress / dawnDuration);
+    settings.daylightValue = lerp(2.3, 1.8, progress);
+    settings.saturation = lerp(0.2, 0.6, progress);
+  }
+  // sunrise
+  else if (
+    new Date() >= new Date(suninfo.sunrise) &&
+    new Date() < new Date(suninfo.sunriseEnd)
+  ) {
+    const sunriseDuration = differenceInSeconds(
+      new Date(suninfo.sunriseEnd),
+      new Date(suninfo.sunrise)
+    );
+    const sunriseProgress = differenceInSeconds(
+      new Date(),
+      new Date(suninfo.sunrise)
+    );
+
+    const progress = eases.cubicOut(sunriseProgress / sunriseDuration);
+
+    settings.daylightValue = lerp(1.8, 1.2, progress);
+    settings.saturation = lerp(0.6, 0.7, progress);
+  }
+  // golden hour
+  else if (
+    new Date() >= new Date(suninfo.sunriseEnd) &&
+    new Date() < new Date(suninfo.goldenHourEnd)
+  ) {
+    settings.daylightValue = 1.2;
+    settings.saturation = 0.7;
+  }
+  // morning
+  else if (
+    new Date() >= new Date(suninfo.goldenHourEnd) &&
+    new Date() < new Date(suninfo.solarNoon)
+  ) {
+    settings.daylightValue = 1.0;
+    settings.saturation = 1.0;
+  }
+  // afternoon
+  else if (
+    new Date() >= new Date(suninfo.solarNoon) &&
+    new Date() < new Date(suninfo.goldenHour)
+  ) {
+    settings.daylightValue = 1.0;
+    settings.saturation = 1.0;
+  }
+  // golden hour
+  else if (
+    new Date() >= new Date(suninfo.goldenHour) &&
+    new Date() < new Date(suninfo.sunsetStart)
+  ) {
+    settings.daylightValue = 1.0;
+    settings.saturation = 1.0;
+  }
+  // sunset
+  else if (
+    new Date() >= new Date(suninfo.sunsetStart) &&
+    new Date() < new Date(suninfo.sunset)
+  ) {
+    const secondsBetween = differenceInSeconds(
+      new Date(suninfo.sunset),
+      new Date(suninfo.sunsetStart)
+    );
+    const secondsOfProgress = differenceInSeconds(
+      new Date(),
+      new Date(suninfo.sunsetStart)
+    );
+    const progress = secondsOfProgress / secondsBetween;
+
+    settings.daylightValue = lerp(1.0, 1.4, progress);
+    settings.saturation = 1.0;
+  }
+  // twilight
+  else if (
+    new Date() >= new Date(suninfo.sunset) &&
+    new Date() < new Date(suninfo.dusk)
+  ) {
+    settings.daylightValue = 1.4;
+    settings.saturation = 0.3;
+  }
+  // dusk
+  else if (
+    new Date() >= new Date(suninfo.dusk) &&
+    new Date() < new Date(suninfo.night)
+  ) {
+    settings.daylightValue = 2.0;
+    settings.saturation = 0.3;
+  }
+  // night
+  else {
+    settings.daylightValue = 2.5;
+    settings.saturation = 0.2;
+  }
+  //
+  // // Cloud Clover
+  // let { cloudCover } = darksky;
+  // let hslLightnessForBg = 95.0;
+  // settings.cloudCover = Math.round((1 - cloudCover) * hslLightnessForBg);
+  //
+  // // Precipitation
+  // let { precipIntensity } = darksky;
+  //
+  // if (precipIntensity === 0) {
+  //   settings.precipIntensity = 1.0;
+  // } else if (precipIntensity < 0.03) {
+  //   settings.precipIntensity = lerp(
+  //     2.8,
+  //     3.0,
+  //     calculateProgress(0, 0.03, precipIntensity)
+  //   );
+  // } else if (precipIntensity >= 0.03 && precipIntensity < 0.1) {
+  //   settings.precipIntensity = lerp(
+  //     3.0,
+  //     4.0,
+  //     calculateProgress(0.03, 0.1, precipIntensity)
+  //   );
+  // } else if (precipIntensity >= 0.1 && precipIntensity < 0.2) {
+  //   let progress = calculateProgress(0.1, 0.2, precipIntensity);
+  //   settings.precipIntensity = lerp(3.0, 3.5, progress);
+  //   settings.noiseMultiplier = lerp(1.5, 2.0, progress);
+  // } else {
+  //   let precipMultiplier = precipIntensity / 0.2;
+  //   settings.precipIntensity = 3.5 * precipMultiplier;
+  //   settings.noiseMultiplier = 2.0 * precipMultiplier;
+  // }
+  //
+  // // Wind Speed
+  // let { windSpeed } = darksky;
+  // if (windSpeed < 5) {
+  //   settings.windSpeed = lerp(0, 0.25, calculateProgress(0, 5, windSpeed));
+  // } else if (windSpeed >= 5 && windSpeed < 10) {
+  //   settings.windSpeed = lerp(0.25, 0.45, calculateProgress(5, 10, windSpeed));
+  // } else if (windSpeed >= 10 && windSpeed < 20) {
+  //   settings.windSpeed = lerp(0.45, 1.0, calculateProgress(10, 20, windSpeed));
+  // } else if (windSpeed >= 20 && windSpeed < 30) {
+  //   settings.windSpeed = lerp(1.0, 2.5, calculateProgress(20, 30, windSpeed));
+  // } else {
+  //   settings.windSpeed = 3.5;
+  //   let windMultiplier = windSpeed / 30;
+  //   settings.windSpeed = 3.5 * windMultiplier;
+  // }
 }
 
 function calculateProgress(minValue, maxValue, currentValue) {
